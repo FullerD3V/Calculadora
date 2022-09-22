17c9d19a22b19c7fe4828df67d139b1d3c47602e
FullerD3V <97800761+FullerD3V@users.noreply.github.com>
2022-09-22 17:34:13 +0200

    Proyecto subido, solo interfaz gráfica

---
+++ src/calculadora/Main/FXMLDocumentController.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.Main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author alumno
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button point, btn1, btn2, btn3, btn4, btn5 ,btn6, btn7, btn8, btn9, btn0, multi, div, minus, plus, equal, reset, bac, history;
+    
+    @FXML
+    private TextField screen;
+    
+    @FXML
+    private void pointOnAction(ActionEvent event) {
+    }
+    
+    @FXML
+    private void btn1OnAction(ActionEvent event) {
+        screen.setText("1");
+    }
+    
+    @FXML
+    private void btn2OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn3OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn4OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn5OnAction(ActionEvent event){
+    
+    }@FXML
+    private void btn6OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn7OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn8OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn9OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void btn0OnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void divOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void multOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void plusOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void minusOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void equalOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void resetOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void backOnAction(ActionEvent event){
+    
+    }
+    @FXML
+    private void historyOnAction(ActionEvent event){
+    
+    }
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
